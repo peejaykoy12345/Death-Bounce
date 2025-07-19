51850ba92a3463fe5714ded05886ba1770958779
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-19 08:25:05 +0800

    Updated dagger to have a poison effect and buffed sword damage

--- Scripts/Dagger.cs
+++ Scripts/Dagger.cs
@@ -1,9 +1,14 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Dagger : WeaponBase
 {
 	private Timer levelTimer;
+
+	// A list for storing every character who is poisoned
+	private List<CharacterBody2D> poison_affected_characters = new List<CharacterBody2D>();
+	private float poison_damage;
 
 	public override void _Ready()
 	{
@@ -17,6 +22,8 @@ public partial class Dagger : WeaponBase
 			damage += 2f * Mathf.Sqrt(level);
 		};
 
+		poison_damage = damage / 4;
+
 		((Area2D)rotator).BodyEntered += (Node2D body) =>
 		{
 			if (body == this) return;
@@ -24,7 +31,33 @@ public partial class Dagger : WeaponBase
 			if (body.HasMethod("TakeDamage"))
 			{
 				body.Call("TakeDamage", damage);
+				ApplyPoison((CharacterBody2D)body);
 			}
 		};
+	}
+
+	private async void ApplyPoison(CharacterBody2D character)
+	{
+		if (poison_affected_characters.Contains(character)) return;
+
+		poison_affected_characters.Add(character);
+		_ = RemovePoisonAfterDelay(character); 
+
+		while (poison_affected_characters.Contains(character))
+		{
+			await ToSignal(GetTree().CreateTimer(1.0), "timeout");
+			GD.Print("This guy is getting brutally murked someone help him");
+
+			if (IsInstanceValid(character))
+			{
+				character.Call("TakeDamage", poison_damage);
+			}
+		}
+	}
+	
+	private async System.Threading.Tasks.Task RemovePoisonAfterDelay(CharacterBody2D character)
+	{
+		await ToSignal(GetTree().CreateTimer(5.0), "timeout");
+		poison_affected_characters.Remove(character);
 	}
 }
