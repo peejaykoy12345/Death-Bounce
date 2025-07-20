910dba2b89fef44bcd63525e10f1f12f6bc3e764
peejaykoy12345 <rick.well.rick@gmail.com>
2025-07-20 21:38:01 +0800

    Updated arena.cs to ref the restart button

--- Scripts/Arena.cs
+++ Scripts/Arena.cs
@@ -4,11 +4,33 @@ using System;
 public partial class Arena : Node2D
 {
 
+	private Button RestartButton;
+
 	private AudioStreamPlayer2D ParrySFX;
 	public override void _Ready()
 	{
 		ParrySFX = GetNode<AudioStreamPlayer2D>("ParrySFX");
+		RestartButton = GetNode<Button>("RestartButton");
+		RestartButton.Visible = false;
+
+		RestartButton.Pressed += () =>GetTree().ReloadCurrentScene();
 	}
+
+	public override void _Process(double delta)
+	{
+		int weapon_count = 0;
+
+		foreach (Node child in GetChildren())
+		{
+			if (child is WeaponBase) weapon_count++;
+		}
+
+		if (weapon_count <= 1)
+		{
+			RestartButton.Visible = true;
+		}
+    }
+
 	public void playParrySound()
 	{
 		if (ParrySFX.Playing) return;
